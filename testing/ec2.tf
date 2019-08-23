2a8acdec81df3da2b362eeaf75b5ca05bf26bdcb
Yoandry Hernandez <yoandryhc@gmail.com>
2019-08-23 15:26:17 -0300

    Actualizado entornos

--- testing/ec2.tf
+++ testing/ec2.tf
@@ -1,5 +1,5 @@
-module "ec2"{
- source = "/home/yhernandez/Terraform/modulos/ec2-with-eip"
+ module "ec2"{
+ source = "github.com/yoandryhc85/curso-terraform-2019?ref=v1.0.2"
  vpc_id = "vpc-7ca8c014"
  project_name = "inStride"
  environment = "Testing"
